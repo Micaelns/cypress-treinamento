bc29b0d67c3087f8bd1a485c45767133b175ad3b
micael.souza <micael.souza@via.com.br>
2022-08-29 10:07:09 -0300

    implements login

--- cypress/e2e/Ui/Login.spec.js
+++ cypress/e2e/Ui/Login.spec.js
@@ -1,9 +1,21 @@
 /// <reference types="cypress" />
 
-describe('TEST001 - Funcionalidade: Login', () => {
-    
-    it('Deve fazer login com sucesso', () => {
-        cy.visit('login');
-    })
+const login ='fabio@bootcamp.com'
+const senha ='teste@123'
 
-})
+describe('TEST001 - Funcionalidade: Login', () => {
+
+    beforeEach(() => {
+        cy.visit('login')
+    });
+
+    it('Deve fazer login com sucesso', () => {
+        cy.login(login, senha)
+        cy.get('[data-test="dashboard-welcome"]').should('contain', 'Bem-vindo Fabio Araújo')
+    });
+
+    it('Validar mensagem de erro quando inserir usuário ou senha inválidos', () => {
+        cy.login('fabioddfd@bootcamp.com', 'testefddfd@123')
+        cy.get('[data-test="alert"]').should('contain', 'Credenciais inválidas')
+    });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,3 +31,10 @@ Cypress.Commands.add('navigate', (route) => {
     cy.visit(route, { timeout: 30000 })
     cy.wait('@loadpage')
 })
+
+
+Cypress.Commands.add("login", (email, senha) => {
+    cy.get('[data-test="login-email"] > .MuiInputBase-root > .MuiInputBase-input').type(email)
+    cy.get('[data-test="login-password"] > .MuiInputBase-root > .MuiInputBase-input').type(senha)
+    cy.get('[data-test="login-submit"]').click()
+ })
